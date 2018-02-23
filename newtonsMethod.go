d218855cb7cc9585e610fad2761e7bd90d358d0c
Joel N. Johnson <joel.johnson675@gmail.com>
2018-02-22 20:36:15 -0700

    Initial commit

---
+++ newtonsMethod.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func Sqrt(x float64) float64 {
+	z := 100.0
+	i := 0
+	for i < 1 {
+		j := math.Abs(z*z - x)
+		if j <= .001 {
+			i = 1
+		} else {
+			fmt.Println(z)
+			z -= (z*z - x)/(2*z)
+		}
+	}
+	return z
+}
+
+func main() {
+	fmt.Println(Sqrt(2))
+}
